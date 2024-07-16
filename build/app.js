d0f8d50bd6fa8a27fef06041bd82a0e140b64da7
micem2 <freakishmice@proton.me>
2024-07-15 21:53:29 -0700

    added morgan and basic cors

--- build/app.js
+++ build/app.js
@@ -4,6 +4,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const morgan_1 = __importDefault(require("morgan"));
 const app = (0, express_1.default)();
+app.use((0, morgan_1.default)('dev'));
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+    res.status(404).end();
+});
 app.listen(3000);
 console.log("Server listening on port 3000");
--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,15 @@
 import express, { NextFunction, Request, Response } from 'express';
+import morgan from 'morgan';
+const cors = require('cors');
 
 const app = express();
 
+app.use(morgan('dev'));
+app.use(cors());
+
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+
 
 app.use((req: Request, res: Response, next: NextFunction) => {
     res.status(404).end();
