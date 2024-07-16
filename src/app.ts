4f306022c0eebff332e8b4219dbd2c271fc516ba
micem2 <freakishmice@proton.me>
2024-07-15 21:42:49 -0700

    setup minimal express

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,13 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 
 const app = express();
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+    res.status(404).end();
+})
 
 app.listen(3000);
 console.log("Server listening on port 3000");
