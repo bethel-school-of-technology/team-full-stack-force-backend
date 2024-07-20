d4e0d3635ad1b44a8f855328b1ab7525a8233a65
micem2 <freakishmice@proton.me>
2024-07-19 20:23:34 -0700

    updated to use mysql db

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import express, { NextFunction, Request, Response } from 'express';
 import morgan from 'morgan';
 const cors = require('cors');
 
+import { db } from './models';
+
 const app = express();
 
 app.use(morgan('dev'));
@@ -13,6 +15,11 @@ app.use(express.urlencoded({extended: true}))
 
 app.use((req: Request, res: Response, next: NextFunction) => {
     res.status(404).end();
+})
+
+db.sync().then(() => {
+    console.log("Connected to MySQL");
+    
 })
 
 app.listen(3000);
