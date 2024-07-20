d575f6a6b7b587a93d7c97636fd7869b462f0690
micem2 <freakishmice@proton.me>
2024-07-19 20:09:22 -0700

    fixed a small mistake

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -7,7 +7,7 @@ export class User extends Model<InferAttributes<User>, InferCreationAttributes<U
     declare email: string;
 }
 
-export function TaskFactory(sequelize: Sequelize) {
+export function UserFactory(sequelize: Sequelize) {
     User.init({
         userId: {
             type: DataTypes.INTEGER,
