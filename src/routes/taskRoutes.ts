780b0adfbf7dfe434866a121b7d632772f0a455c
micem2 <freakishmice@proton.me>
2024-07-23 16:24:44 -0700

    commented out temp

--- src/routes/taskRoutes.ts
+++ src/routes/taskRoutes.ts
@@ -2,11 +2,11 @@ import { Router } from "express";
 
 const router = Router();
 
-router.get('/', getAllTasks);
-router.get('/:id', getTaskById);
-router.get('/:user', getTaskByUser);
-router.post('/', addTask);
-router.put('/:id', editTask);
-router.delete('/:id', deleteTask);
+// router.get('/', getAllTasks);
+// router.get('/:id', getTaskById);
+// router.get('/:user', getTaskByUser);
+// router.post('/', addTask);
+// router.put('/:id', editTask);
+// router.delete('/:id', deleteTask);
 
 export default router;
