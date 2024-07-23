e1c495d887a7fb63f5987aa17d41d59118004510
micem2 <freakishmice@proton.me>
2024-07-22 19:41:27 -0700

    imported routes

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import express, { NextFunction, Request, Response } from 'express';
 import morgan from 'morgan';
 const cors = require('cors');
+import taskRoutes from './routes/taskRoutes';
+import userRoutes from './routes/userRoutes';
 
 import { db } from './models';
 
@@ -12,7 +14,11 @@ app.use(cors());
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
+// routes
+app.use('/api/tasks', taskRoutes);
+app.use('/api/users', userRoutes);
 
+// 404 if not found
 app.use((req: Request, res: Response, next: NextFunction) => {
     res.status(404).end();
 })
