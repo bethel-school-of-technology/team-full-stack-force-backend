2c52cfe946f49ca29206e068b0e499650e18e868
micem2 <freakishmice@proton.me>
2024-07-23 16:23:33 -0700

    commented out temp

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,11 +1,11 @@
 import { Router } from "express";
-import { getUserById } from "../controllers/userController";
+import { addUser, editUser, getUserById } from "../controllers/userController";
 
 const router = Router();
 
 router.get('/:id', getUserById);
 router.put('/:id', editUser);
 router.post('/', addUser);
-router.post('/login', loginUser);
+// router.post('/login', loginUser);
 
 export default router;
