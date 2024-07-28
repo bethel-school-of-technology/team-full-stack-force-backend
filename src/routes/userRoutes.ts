cde57161f26a532b750b639a900c718e1a58bf16
micem2 <freakishmice@proton.me>
2024-07-27 20:32:17 -0700

    make login work

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,11 +1,11 @@
 import { Router } from "express";
-import { addUser, editUser, getUserById } from "../controllers/userController";
+import { addUser, editUser, getUserById, loginUser } from "../controllers/userController";
 
 const router = Router();
 
 router.get('/:id', getUserById);
 router.put('/:id', editUser);
 router.post('/', addUser);
-// router.post('/login', loginUser);
+router.post('/login', loginUser);
 
 export default router;
