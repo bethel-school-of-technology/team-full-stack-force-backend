b4a36fe7dbc703ddae3b77957d526db6cf7cb779
micem2 <freakishmice@proton.me>
2024-07-31 23:29:58 -0700

    adding user is now /register

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -5,7 +5,7 @@ const router = Router();
 
 router.get('/:id', getUserById);
 router.put('/:id', editUser);
-router.post('/', addUser);
+router.post('/register', addUser);
 router.post('/login', loginUser);
 
 export default router;
