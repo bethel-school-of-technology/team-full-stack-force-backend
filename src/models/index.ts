df22ddd723cee789eb5ba073c7ff940b31ce1915
nathanwmurray <nwilliammurray@gmail.com>
2024-08-06 19:21:51 -0500

    Set up server online

--- src/models/index.ts
+++ src/models/index.ts
@@ -3,12 +3,12 @@ import { AssociateUserMessage, TaskFactory } from "./taskModel"
 import { UserFactory } from "./userModel"
 
 
-const dbName = 'taskmanagerdb';
-const username = 'root'; 
-const password = 'changeme'; // change password if necessary
+const dbName = 'sql5724433';
+const username = 'sql5724433'; 
+const password = 'FpXyFP4PI7'; // change password if necessary
 
 const sequelize = new Sequelize(dbName, username, password, {
-    host: 'localhost',
+    host: 'sql5.freemysqlhosting.net',
     port: 3306, //change port if necessary
     dialect: 'mysql'
 });
